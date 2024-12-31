a2b016aed35d3d004870fb1360b02b9cb226616b
Matthew Elliott <m.elliott091091@gmail.com>
2024-12-31 18:09:23 +0000

    Add project files.

---
+++ Small Quiz App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Small_Quiz_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
